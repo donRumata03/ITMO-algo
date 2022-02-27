f72053ff35cff9e52109b975f9825da7504b4f27
donRumata <donrumata03@gmail.com>
2022-02-27 22:30:17 +0300

    segment_tree: Introduce some abstractions…

--- segment-tree/src/A.rs
+++ segment-tree/src/A.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Range};
 use std::cmp;
 use std::marker::PhantomData;
 
@@ -7,6 +7,9 @@ trait ReductionOp<T> {
 	fn neutral() -> T;
 	fn apply(left: T, right: T) -> T;
 }
+
+trait CommutativeOp<T>: ReductionOp<T> {}
+trait DistributiveRelativeTo<T>: ReductionOp<T> {}
 
 
 struct SumReduction<T: From<i64> + Add<Output=T>> { _p: PhantomData<T> }
@@ -20,6 +23,51 @@ impl<T: From<i64> + Add<Output=T>> ReductionOp<T> for SumReduction<T> {
 		left + right
 	}
 }
+
+///_____________________________________________________________________________
+
+trait ModificationQuery {}
+trait ReductionQuery {}
+
+struct ElementModificationQuery<Element> {
+	position: usize,
+	new_element: Element,
+}
+
+impl ModificationQuery for ElementModificationQuery<Element> {
+	
+}
+
+struct SegmentModificationQuery<Element> {
+	segment: Range<usize>,
+	new_element: Element,
+}
+
+struct ElementReductionQuery<Element> {
+	element: usize
+}
+
+struct SegmentReductionQuery<Element> {
+	segment: Range<usize>
+}
+
+
+trait SegmentTree<ModificationQuery> {
+
+}
+
+///_____________________________________________________________________________
+
+struct SegmentTreeEngine<Element> {
+	data: Vec<Element>
+}
+
+impl<Element> SegmentTreeEngine<Element> {
+	
+}
+
+
+/// _____________________________________________________________________________
 
 
 fn main() {
